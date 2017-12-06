dcd5b2fda6cd797f13a042154e8a4ecf30a38681
Unknown <christophe.corneau@gmail.com>
2017-12-06 17:05:57 +0100

    New version snake with table HTML + Jquery

--- snake.js
+++ snake.js
@@ -1,136 +1,117 @@
-var size = 30; // px
-var nRow = 20;
-var nCol = 33;
-var snakeplus;
+var map = 40;
+var snakeHead = [0,5];
+var snakeBody = [
+    [0,5],[0,4],[0,3],[0,2],[0,1] 
+];
+var direction = "droite";
+var speed = 100;
+var score = 0;
+var snakeSize = snakeBody.length;
 
-$(document).ready(function () { // quand le HTML est chargé
-  console.log("ready!");
-  init()
 
-});
-//Initialisatoin du jeu
-function init() {
-  //snakeplus();
-  //createApple();
-  positionApple();
-}
-//Placement de la pomme de facon aléatoire
-function positionApple() {
-  console.log('positionApple')
-  var x = Math.round(Math.random() * nCol) * size;
-  var y = Math.round(Math.random() * nRow) * size;
-  var $apple = $('.apple'); // document.querySelector('.apple')
-  $apple.css({
-    'top': y + 'px',
-    'left': x + 'px',
-  });
-}
-
-/*function snakeplus() {
-  var length = 5; //Length of the snake
-  snakeplus = [].insertAdjacentHTML('afterbegin', '<div class="snake"></div>'); //Empty array to start with
-  for (var i = length +1; i >= 0; i++) {
-    //This will create a horizontal snake starting from the top left
-    snakeplus.push({ x: i, y: 0 });
-    console.log("snakeplus");
-  }
-}
-snakeplus();
-*/
-//Déclaration des touches de direction pour controler le snake
-var TOP = 38, BOTTOM = 40, LEFT = 37, RIGHT = 39;
-
-$(document).keydown(function (e) {
-  console.log('e:', e);
-  var snake = $('.snake');
-  var position = snake.position();
-  switch (e.keyCode) {
-    case BOTTOM:
-      var top = position.top + 30;
-      snake.css({ 'top': top + 'px' });
-      if (checkColision()) snake.css({ 'top': (top - 30) + 'px' });
-      break;
-    case TOP:
-      var top = position.top - 30;
-      snake.css({ 'top': top + 'px' });
-      if (checkColision()) snake.css({ 'top': (top + 30) + 'px' });
-      break;
-    case LEFT:
-      var left = position.left - 30;
-      snake.css({ 'left': left + 'px' });
-      if (checkColision()) snake.css({ 'left': (left + 30) + 'px' });
-      break;
-    case RIGHT:
-      var left = position.left + 30;
-      snake.css({ 'left': left + 'px' });
-      if (checkColision()) snake.css({ 'left': (left - 30) + 'px' });
-      break;
-  }
+$(document).ready(function(){
+    createMap();
+    startGame();
+    move();
+    createApple();
+      
 });
 
-function checkColision() {
-  var boum = true;
-  $('#game').each(function () {
-    if (colision($('.snake'), $(this))) {
-      boum = false;
-      return;
+function createMap() {
+    var td ="";
+    var tr = [];
+    for(i=0;i<map;i++) {
+        td += "<td></td>";
     }
-  });
-
-  return boum;
-}
-
-function colision(elt1, elt2) {
-  var p_elt1 = elt1.position();
-  var p_elt2 = elt2.position();
-
-  if (p_elt1.left < p_elt2.left + elt2.width() &&
-    p_elt1.left + elt1.width() > p_elt2.left &&
-    p_elt1.top < p_elt2.top + elt2.height() &&
-    elt1.height() + p_elt1.top > p_elt2.top) {
-    return true;
-  }
-}
-
-/*
-//Récupération des données du canvas HTML
-var canvas = $('canvas');
-var context = canvas[0].getContext('2d');
-var width = $('canvas').width();
-var height = $('canvas').height();
-
-
-//Création du snake
-function createSnake(){
-    var snake = [{
-        var length = ["","","",""];
-        context.fillStyle = "green";
-        context.fillRect(0, 0, 30, 30);
-   }]
-
-}
-//Creation de la pomme 
-function createApple(){
-    var apple = [{
-        context.fillStyle = "red";
-        context.fillRect(0, 0, 30, 30);
-    ]}
-}
-//Placement de la pomme de facon aléatoire
-function positionApple(){
-    var x = Math.random(Math.round());
-    var y = Math.random(Math.round());
-    snake.css({'left': x + 'px'}
-    snake.css({'top': y + 'px'}
-}
-//Mange pomme +1 pour la variable serpent
-function eatApple(){
-    if (position.snake == position.apple) {
-        snake++
+    for(i=0;i<map;i++) {
+        tr.push("<tr>"+td+"</tr>");
     }
+    $(document.body).append("<table>"+tr.join('\n')+"</table>");
 }
-//Colission
-function colission (){
 
+function createSnake() {
+    $('td').removeClass('snakeHead snakeBody');
+    for (var cell in snakeBody) {
+        $('tr').eq(snakeBody[cell][0]).find('td').eq(snakeBody[cell][1]).addClass('snakeBody')
+    }
+        $('tr').eq(snakeHead[0]).find('td').eq(snakeHead[1]).addClass('snakeHead')
 }
-*/
+
+function actualitySnake() {
+    var newHead = [];
+    if (direction == "droite") {
+        newHead = [snakeHead[0],snakeHead[1]+1];
+    }
+    else if (direction == "gauche") {
+        newHead = [snakeHead[0],snakeHead[1]-1];
+    }
+    else if (direction == "haut") {
+        newHead = [snakeHead[0]-1,snakeHead[1]];
+    }
+    else if (direction == "bas") {
+        newHead = [snakeHead[0]+1,snakeHead[1]];
+    }
+
+    cell = $('tr').eq(newHead[0]).find('td').eq(newHead[1]);
+    if (cell.hasClass('fruit')) {
+        snakeBody.push([]);
+        createApple();
+        score+=100;
+        $('#score span').html(''+score);
+    }
+    else if (cell.hasClass('snakebody')) {
+        gameOver();
+    }
+    else if (newHead[0]<0 || newHead[1]<0) {
+        gameOver();
+    }
+else if (newHead[0]>map || newHead[1]>map) {
+    gameOver();
+}
+    for(i=snakeSize-1;i>0;i--) {
+        snakeBody[i] = snakeBody[i-1];
+    }
+    snakeBody[0] = snakeHead = newHead;
+    createSnake();
+}
+// vitesse de deplacement du snake
+function startGame (){
+    go = setInterval(actualitySnake,speed);
+}
+
+//deplacement snake
+function move() {
+    var gauche = 37, haut = 38, droite = 39, bas = 40;
+    //Detection des touches
+    $('body').keydown(function(e){
+        //alert(e.keyCode);  
+    if(e.keyCode == gauche) {
+        direction ="gauche";
+    }
+    else if(e.keyCode == droite) {
+        direction ="droite";
+    }
+    else if(e.keyCode == haut) {
+        direction ="haut";
+    }
+    else if(e.keyCode == bas) {
+        direction ="bas";
+    }})
+}
+
+//creation de la pomme
+function createApple() {
+    $('td').removeClass('fruit');
+    fruit = [parseInt(Math.random()*map),parseInt(Math.random()*map)];
+    $('tr').eq(fruit[0]).find('td').eq(fruit[1]).addClass('fruit');
+    
+    //return parseInt(Math.random()*map); 
+}
+//gameover
+function gameOver() {
+    $('#gameover').show('fast',function(){
+        $(this).animate({top:100},'slow');
+    })
+    clearInterval(go);
+    //die();
+}
